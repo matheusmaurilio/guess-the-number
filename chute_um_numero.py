17bc4c4739ed2022257ca036b61b783d7cd2c74c
Matheus Maurilio <mathmaurilio@gmail.com>
2022-06-27 14:52:30 -0300

    Versão 1.01 com interface GUI

--- chute_um_numero.py
+++ chute_um_numero.py
@@ -1,6 +1,7 @@
 #Chute um número até acertar !
 
 import random
+import PySimpleGUI as PSG
 
 class ChuteONumero:
     def __init__(self):
@@ -10,26 +11,41 @@ class ChuteONumero:
         self.tentar_novamente = True
     
     def Iniciar(self):
+        #Layout
+        layout = [
+            [PSG.Text('Seu chute: ', size=(20,0))],
+            [PSG.Input(size=(18,0), key='ValorChute')],
+            [PSG.Button('Chutar')],
+            [PSG.Output(size=(30,10))]
+        ]
+        #Janela
+        self.janela = PSG.Window('Chute o numero! ', layout)
         self.GerarNumeroAleatorio()
-        self.PedirValorAleatorio()
+
         try:
-            while self.tentar_novamente:
-                if int(self.valor_do_chute) > self.valor_aleatorio:
-                    print('Chute um valor menor! ')
-                    self.PedirValorAleatorio()
-                elif int(self.valor_do_chute) < self.valor_aleatorio:
-                    print('Chute um valor maior! ')
-                    self.PedirValorAleatorio()
-                elif int(self.valor_do_chute) == self.valor_aleatorio:
-                    print('Parabéns, você acertou! ')
-                    self.tentar_novamente = False
+            while True:   
+                #Receber os valores
+                self.eventos, self.valores = self.janela.Read()
+                if self.eventos == 'Chutar':
+                    self.valor_do_chute = self.valores['ValorChute']
+                    while self.tentar_novamente:
+                        if int(self.valor_do_chute) > self.valor_aleatorio:
+                            print('Chute um valor menor! ')
+                            break
+                        elif int(self.valor_do_chute) < self.valor_aleatorio:
+                            print('Chute um valor maior! ')
+                            break
+                        if int(self.valor_do_chute) == self.valor_aleatorio:
+                            print('Parabéns, você acertou! ')
+                            self.tentar_novamente = False
+                            break
+                        # Window close button event
+                elif self.eventos == PSG.WIN_CLOSED:    
+                    break
         except:
             print('Chute um valor válido (1 a 100)')
             self.Iniciar()
    
-    def PedirValorAleatorio(self):
-        self.valor_do_chute = int(input('Chute um número entre 1 e 100: '))
-        
     def GerarNumeroAleatorio(self):
         self.valor_aleatorio = random.randint(self.valor_minimo, self.valor_maximo)
 
